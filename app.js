4ac0220bb23b0f9b408dadaa53e0e4afe899d85a
chvsai <chadaramlakshmi@gmail.com>
2023-09-06 14:34:34 +0900

    gitignore and pkg

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+const fs = require('fs')
+const http = require('http')
+
+// console log and application initial setup check
+
 const welcome_text = "Hello"
 console.log(welcome_text)
+
+// create simple server
+
+const server = http.createServer((req,res) => {
+    console.log(req)
+res.end("hello from server")
+})
+
+server.listen(8000,"127.0.0.1",()=>{
+    
+    console.log("listening for requests")
+})
+
